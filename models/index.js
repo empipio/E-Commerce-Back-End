d928b85c9b1fac3cdb58355b2a6872b606d2d3d7
empipio <94937507+empipio@users.noreply.github.com>
2022-03-20 21:17:25 +0000

    starter code copied, models done

---
+++ models/index.js
@@ -0,0 +1,24 @@
+// import models
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
+
+// Products belongsTo Category
+
+// Categories have many Products
+
+// Products belongToMany Tags (through ProductTag)
+
+// Tags belongToMany Products (through ProductTag)
+
+// Allow products to have multiple tags and tags to have many products by using the ProductTag through model.
+
+//Hint: Make sure you set up foreign key relationships that match the column we created in the respective models.
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const routes = require("./routes");
+const sequelize = require("./config/connection");
+// import sequelize connection
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
+});
+
+/* 
+Schema.sql file-don't think need to do anything here  //
+.env file, add to .gitgnore  //
+models according to readme instructions //
+relationships in models/index
+routes for category/product/tag CRUD
+Create the code needed in server.js to sync the Sequelize models to the MySQL database on server start  //
+*/
